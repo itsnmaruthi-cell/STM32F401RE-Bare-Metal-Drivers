37cbf87a77d5839f3239745f4eb067ace285eea7
itsnmaruthi-cell <itsnmaruthi@gmail.com>
2026-07-01 12:58:56 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,210 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdint.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+//***********GPIOA RELATED POINTERS********
+volatile uint32_t* RCC_AHB1ENR=(volatile uint32_t*)0X40023800;
+volatile uint32_t* GPIOA_MODER=(volatile uint32_t*)0X40020000;
+volatile uint32_t* GPIOA_BSRR=(volatile uint32_t*)0X40020018;
+volatile uint32_t* GPIOA_AFRL=(volatile uint32_t*)0x40020020;
+
+//************TIM2 RELATED POINTERS***********
+volatile uint32_t* RCC_APB1ENR=(volatile uint32_t*)0X40023840;
+volatile uint32_t* TIM2_CR1=(volatile uint32_t*)0X40000000;
+volatile uint32_t* TIM2_DIER=(volatile uint32_t*)0X4000000C;
+volatile uint32_t* TIM2_SR=(volatile uint32_t*)0X40000010;
+volatile uint32_t* TIM2_PSC=(volatile uint32_t*)0X40000028;
+volatile uint32_t* TIM2_ARR=(volatile uint32_t*)0X4000002C;
+volatile uint32_t* TIM2_CCR1=(volatile uint32_t*)0X40000034;
+volatile uint32_t* TIM2_CCER=(volatile uint32_t*)0X40000020;
+volatile uint32_t* TIM2_CCMR1=(volatile uint32_t*)0X40000018;
+
+//**********NVIC POINTER*****
+volatile uint32_t* NVIC_ISER0=(volatile uint32_t*)0XE000E100;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  /* USER CODE BEGIN 2 */
+  *RCC_AHB1ENR|=(1U<<0);//CLOCK ENABLE FOR GPIOA
+  *RCC_APB1ENR|=(1U<<0);//TIM2 CLOCK ENABLE
+  *GPIOA_MODER&=~(3U<<10);
+  *GPIOA_MODER|=(1U<<11);//PA5 ALTERNATE FUNCTION MODE
+  *GPIOA_AFRL&=~((1U<<20)|(1U<<21)|(1U<<22)|(1U<<23));
+  *GPIOA_AFRL|=(1U<<20);
+  *NVIC_ISER0|=(1U<<28);//TIM2 INTERRUPTS ENABLED TO MANAGE
+  *TIM2_PSC=0X00015999;
+  *TIM2_ARR=0X00000999;
+  *TIM2_CCR1=0X00000500;//50% DUTY CYCLE
+  *TIM2_CCMR1|=(6U<<4);
+  *TIM2_CCER|=(1U<<0);
+  *TIM2_CCMR1|=(1U<<3);
+  *TIM2_CR1|=(1U<<0);
+  *GPIOA_BSRR |= (1U<<5);   // force PA5 HIGH directly
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
